6f262683e9f4ec1c42cb61ef23c01cebd8e997ee
Kim, Kai <150426844+KrabbyKai@users.noreply.github.com>
2026-03-22 14:39:13 -0700

    Introduction Added
    Printed the introduction underneath the students name.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,12 @@ def main():
     name = "Kai Kim" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("""
+Hello Everyone! My name is Kai, and I'm a junior at the Science Academy STEM Magnet 
+in North Hollywood California. I do FIRST Robotics, and I'm the captain of my rookie team 
+Hypernova Robotics 11024. I also love flying drones, and I actually had the opportunity to 
+compete at the highest level and place 2nd in the world in a team relay competition. 
+It's largely the reasons for me being so late to join the CSS course party. 
+I hope to learn a lot from this course and hopefully be of use to everyone here!""")
 if __name__ == "__main__":
     main()
